7476921f782829b7c7a9f1ba1e29f34b063985e9
canozdemir <canozdemir92@gmail.com>
2022-06-23 20:43:40 +0300

    Created personal web-site and add free theme

---
+++ Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using Entity.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Controllers
+{
+    public class MainController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+
+        public IActionResult Index()
+        {
+            var about = db.Abouts.FirstOrDefault();
+            var contactForms = db.ContactForms.FirstOrDefault();
+            var mainPages = db.MainPages.FirstOrDefault();
+            var skills = db.Skills.ToList();
+            var onePageTuple = (about, contactForms, mainPages, skills);
+            return View(onePageTuple);
+        }
+
+        public PartialViewResult MainPage()
+        {
+            //Sends Main Page Partial
+            return PartialView();
+        }
+
+
+        //public IActionResult Deneme()
+        //{
+        //    var skill = db.Skills.ToList();
+        //    return PartialView(skill);
+        //}
+
+      
+    }
+}
